70b364e0d13d2b6ecf25fa72f032cf43cd043148
(no author) <(no author)@531253f7-f1de-0310-85f3-ef76114adc65>
2009-03-22 14:48:19 +0000

    Reformatted.

--- article/code/insert-without-quotes.rb
+++ article/code/insert-without-quotes.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 require 'sqlite3'
 
-print("Enter a name to insert:\n")
+print( "Enter a name to insert:\n" )
 name = gets()
 db = SQLite3::Database.new( "injection1.db" )
-db.execute("INSERT INTO names (id, name) VALUES (null, " + name + ")");
+db.execute( 
+           "INSERT INTO names (id, name) VALUES (null, " + name + ")" 
+          )
