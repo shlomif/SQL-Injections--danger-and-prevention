bf095fbebf2edae9effa4b00eace7a54d6a71c6a
(no author) <(no author)@531253f7-f1de-0310-85f3-ef76114adc65>
2009-03-25 16:36:08 +0000

    Updated.

--- article/code/insert-with-quotes.rb
+++ article/code/insert-with-quotes.rb
@@ -11,6 +11,6 @@ print( "Enter a name to insert:\n" )
 name = input()
 
 db = SQLite3::Database.new( "injection1.db" )
-db.execute( 
+db.execute_batch( 
     "INSERT INTO names (id, name) VALUES (null, " + "'" + name + "'" + ")" 
 )
--- article/code/insert-without-quotes.rb
+++ article/code/insert-without-quotes.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 require 'sqlite3'
 
+def input
+    ret = gets()
+    ret.chomp!()
+    return ret
+end
+
 print( "Enter a name to insert:\n" )
-name = gets()
+name = input()
 db = SQLite3::Database.new( "injection1.db" )
-db.execute( 
+db.execute_batch( 
            "INSERT INTO names (id, name) VALUES (null, " + name + ")" 
           )
